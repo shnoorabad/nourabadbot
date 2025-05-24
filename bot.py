f5279914d93572b06548f9508f6e51415bf3b93d
shnoorabad <amasomi_376@yahoo.com>
2025-05-25 02:36:40 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -324,8 +324,8 @@ async def ask_end(update: Update, context: ContextTypes.DEFAULT_TYPE):
     start = shamsi_to_miladi(admin_report_requests[user_id]["start"])
     end = shamsi_to_miladi(update.message.text.strip())
 if not start or not end:
-     await update.message.reply_text("تاریخ شروع یا پایان معتبر نیست.")
-     return ConversationHandler.END
+    await update.message.reply_text("تاریخ شروع یا پایان معتبر نیست.")
+    return ConversationHandler.END
     conn = sqlite3.connect(DB_FILE)
     cursor = conn.cursor()
     start_datetime = datetime.combine(start, time(0, 0)).isoformat()
